af8dee64c86d156c44e8426a64cae87588312054
Ashokkumar R <r.ashokkumar@nagarro.com>
2025-01-12 21:44:15 +0530

    env variable added and applied the same inside routes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    mfeInsuranceDomain: 'https://abc-insurance-mfe1.netlify.app',  // Production domain
+    mfePaymentDomain: 'https://abc-insurance-mfe2.netlify.app',
+    webHostDomain: 'https://abc-insurance.netlify.app',
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    mfeInsuranceDomain: 'http://localhost:4201',  // Local domain for development
+    mfePaymentDomain: 'http://localhost:4202',
+    webHostDomain: 'http://localhost:4200',
+  };
+  
